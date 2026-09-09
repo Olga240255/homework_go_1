bce26f848e55347749a3fd24f4b35a4ba04af52c
Ольга Макарова <olgamakarova@MacBook-Air-Olga.local>
2026-09-09 21:10:52 +0500

    homework check 1

--- internal/homework/homework.go
+++ internal/homework/homework.go
@@ -1,12 +1,14 @@
 package homework
 
+import "fmt"
+
 // BuildGreeting возвращает приветствие для ученика.
 //
 // Ожидаемый формат:
 // "Привет, {name}! Добро пожаловать в Go."
 func BuildGreeting(name string) string {
 	// TODO: реализуйте функцию через fmt.Sprintf.
-	return ""
+	return fmt.Sprintf("Привет, %s! Добро пожаловать в Go.", name)
 }
 
 // BuildCourseWelcome возвращает название курса.
@@ -15,7 +17,7 @@ func BuildGreeting(name string) string {
 // "Курс: {courseName}"
 func BuildCourseWelcome(courseName string) string {
 	// TODO: реализуйте функцию через fmt.Sprintf.
-	return ""
+	return fmt.Sprintf("Курс: %s", courseName)
 }
 
 // BuildLessonTitle возвращает название первого урока.
@@ -24,7 +26,7 @@ func BuildCourseWelcome(courseName string) string {
 // "Урок 1: {lessonName}"
 func BuildLessonTitle(lessonName string) string {
 	// TODO: реализуйте функцию через fmt.Sprintf.
-	return ""
+	return fmt.Sprintf("Урок 1:  %s", lessonName)
 }
 
 // BuildRepositoryPath возвращает путь до репозитория на GitHub.
@@ -32,8 +34,8 @@ func BuildLessonTitle(lessonName string) string {
 // Ожидаемый формат:
 // "github.com/{owner}/{repo}"
 func BuildRepositoryPath(owner string, repo string) string {
-	// TODO: реализуйте функцию через fmt.Sprintf.
-	return ""
+
+	return fmt.Sprintf("github.com/%s/%s", owner, repo)
 }
 
 // BuildRunCommand возвращает команду запуска Go-программы.
@@ -42,5 +44,5 @@ func BuildRepositoryPath(owner string, repo string) string {
 // "go run {packagePath}"
 func BuildRunCommand(packagePath string) string {
 	// TODO: реализуйте функцию через fmt.Sprintf.
-	return ""
+	return fmt.Sprintf("go run %s", packagePath)
 }
